f85707bbebc0c9b697f13b92ce844a684cfd63cf
babybunso <rodelarenas@gmail.com>
2026-02-21 17:21:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ import streamlit as st
 
 st.write('# Elijah idol kita')
 st.caption('HIIHIHIHIIH')
+st.write('# This is streamlit')
+st.caption('hello, python')
 
 st.write('### This website is under construction...')
