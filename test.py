35cc55094e6cf5992f53359b90737fb150103cec
babybunso <rodelarenas@gmail.com>
2026-02-21 15:35:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 import streamlit as st 
 
-st.write('This is streamlit')
-print('jhello, python')
+st.write('# This is streamlit')
+st.caption('jhello, python')
+
+st.write('### This website is under construction...')
